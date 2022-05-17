630376c9703b05b168aed65e38f7c0c4b8508647
Henry_DelGlitch <CodingWHenry@gmail.com>
2022-05-17 00:35:03 -0400

    Version 2

--- src/components/AI/Ai.js
+++ src/components/AI/Ai.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react'
 import Response from './../Response/Response';
 import './Ai.css'
+const API_KEY = `${process.env.REACT_APP_API_KEY}`;
 
 export function Ai () {
     const [input, setInput] = useState('')
     const [prompt, setPrompt] = useState([])
     const [response, setResponse] = useState([])
+    const [loading, setLoading] = useState(false)
 
     const getResponse = async() => {
+        setLoading(true)
+        setInput('')
         await fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
         method: "POST",
         headers: {
@@ -23,9 +27,13 @@ export function Ai () {
         .then(response => response.json())
         .then(setPrompt(state => [...state, input]))
         .then(data => setResponse(state => [...state, data.choices[0].text]))
+        setLoading(false)
+        window.sessionStorage.setItem('prompt', prompt)
+        window.sessionStorage.setItem('response', response)
     }
 
     const handleChange = (e) => {
+        e.preventDefault()
         setInput(e.target.value)
     }
 
@@ -33,19 +41,18 @@ export function Ai () {
         <div>
             <h1 className='App-title'>Fun with AI</h1>
 
-            <p className='prompt-title'>Enter prompt</p>
-            <form className='prompt'>
-                <input 
+            <p className='prompt-title'><i>Enter prompt</i></p>
+                <textarea 
                     type="text" 
                     id="prompt" 
-                    name="prompt" 
-                    size="50" 
+                    name="prompt"
+                    placeholder='Say this is a test'
                     value={input}
                     onChange={e => handleChange(e)}
                 />
-            </form>
             <input type="submit" value="Submit" className="prompt-button" onClick={getResponse} />
-            <h2>Responses</h2>
+            <h2 className='response-title'>Responses</h2>
+            {loading ? <div id='loading'><p>Loading....</p></div> : <></>}
             {response.map((r, id) => (
                 <Response 
                     prompt={prompt[id]}
