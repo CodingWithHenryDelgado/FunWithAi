32e5ff67ecec88b652c6e70f11486c1776574262
Henry_DelGlitch <CodingWHenry@gmail.com>
2023-02-14 18:58:50 -0500

    Added icons to the AI response. Updated styling for tablet screens for Chat and Input Area components.

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Chat.css'
+import ROBOT from '../../images/robot-icon.png';
 
 const Chat = (props) => {
     const COLORS = [
@@ -7,15 +8,24 @@ const Chat = (props) => {
         "chat-gray"
     ];
 
+    const ICONS = [
+        "none",
+        "block"
+    ]
+
     //The AI response's are in blue
     const getChatColor = (index) => COLORS[index % 2];
+    const getChatIcon = (index) => ICONS[index % 2];
 
     return (
         <>
             {props.chat.map((p, index) => (
-                <li className={`chat-box ${getChatColor(index)}`} key={index}>
-                    {p}
-                </li>
+                <div className='new-chat-line'>
+                    <img className="chat-icon" src={ROBOT} key={'Picture' + index} alt="Robot Icon" style={{ display: getChatIcon(index) }} />
+                    <li className={`chat-box ${getChatColor(index)}`} key={index}>
+                        {p}
+                    </li>
+                </div>
             ))}
         </>
     )
